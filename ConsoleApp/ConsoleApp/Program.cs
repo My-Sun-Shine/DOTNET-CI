73d0db003348739e7dfe98d34e56ab4b57dbb0f6
My-Sun-Shine <1399237176@qq.com>
2019-06-11 14:18:47 +0800

    提交
    Signed-off-by: My-Sun-Shine <1399237176@qq.com>

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -26,10 +26,29 @@ namespace ConsoleApp
                 
             Console.Write("Hello,World");
             Console.ReadKey();
+            Main_1();
         }
         private static bool HasContent2(List<string> strings)
         {
             return strings.Count() >= 1;  // Noncompliant
+        }
+        static void Main_1()
+        {
+            int await = 42;
+            string i = "1";
+            string j = "1";
+            List<string> lst = new List<string>();
+            if (i == j)
+            {
+                Console.Write("345");
+            }
+            if (HasContent2(lst))
+            {
+                Console.Write("123");
+            }
+
+            Console.Write("Hello,World");
+            Console.ReadKey();
         }
     }
 }
